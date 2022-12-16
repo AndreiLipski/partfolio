9c6c351993126a34f02e1f2ec164fa558a8ed64a
Andrei L <davinchi.l.a.s@mail.ru>
2022-12-16 17:27:51 +0300

    new hamburger

--- js/burger.js
+++ js/burger.js
@@ -1,18 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.nav-menu'),
-    menuItem = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger');
+// window.addEventListener('DOMContentLoaded', () => {
+//     const menu = document.querySelector('.nav-menu'),
+//     menuItem = document.querySelectorAll('.nav-menu__list-item'),
+//     hamburger = document.querySelector('.hamburger');
   
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('nav-menu_active');
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('hamburger_active');
+//         menu.classList.toggle('nav-menu_active');
         
-    });
+//     });
   
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('nav-menu_active');
-        })
-    })
-  })
+//     menuItem.forEach(item => {
+//         item.addEventListener('click', () => {
+//             hamburger.classList.toggle('hamburger_active');
+//             menu.classList.toggle('nav-menu_active');
+//         })
+//     })
+//   })
+
+const menu = document.querySelector('.nav-menu');
+const hamb = document.querySelector('.hamburger');
+
+document.addEventListener('click', showHamb);
+
+function showHamb(event) {
+    if (event.target.closest('.hamburger')){
+        menu.classList.toggle('nav-menu_active');
+        hamb.classList.toggle('hamburger_active');
+    }
+    if (!event.target.closest(".hamburger")){
+        hamb.classList.remove("hamburger_active");
+        menu.classList.remove("nav-menu_active");
+    }
+}
