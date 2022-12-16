aed84709668b44ff0775d51ea0ce95c0c138253a
Andrei L <davinchi.l.a.s@mail.ru>
2022-12-16 18:10:02 +0300

    hamb night

--- js/burger.js
+++ js/burger.js
@@ -19,6 +19,8 @@
 
 const menu = document.querySelector('.nav-menu');
 const hamb = document.querySelector('.hamburger');
+const night = document.querySelector('.hamb-night');
+const body = document.querySelector('.body');
 
 document.addEventListener('click', showHamb);
 
@@ -26,9 +28,13 @@ function showHamb(event) {
     if (event.target.closest('.hamburger')){
         menu.classList.toggle('nav-menu_active');
         hamb.classList.toggle('hamburger_active');
+        night.classList.toggle('hamb-night_active');
+        body.classList.toggle('body_active');
     }
     if (!event.target.closest(".hamburger")){
         hamb.classList.remove("hamburger_active");
         menu.classList.remove("nav-menu_active");
+        night.classList.remove('hamb-night_active');
+        body.classList.remove('body_active');
     }
 }
